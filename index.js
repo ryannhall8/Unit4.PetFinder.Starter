2f1ea2648b19a94cc3119af42880cabdbf509ba1
Ryann Hall <ryannhall@Ryanns-MacBook-Air.local>
2024-01-25 17:33:38 -0500

    finished workshop

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 // import the pets array from data.js
+
 const pets = require('./data');
 
 // init express app
@@ -10,7 +11,7 @@ const PORT = 8080;
 // GET - / - returns homepage
 app.get('/', (req, res) => {
     // serve up the public folder as static index.html file
-
+    res.sendFile(_dirname + '/public/index.html');
 });
 
 // hello world route
@@ -21,17 +22,17 @@ app.get('/api', (req, res) => {
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
     // send the pets array as a response
-
+    res.send(pet);
 });
 
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const {owner} = req.query;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
-
+    res.send(pet);
     // send the pet as a response
 
 });
@@ -39,11 +40,10 @@ app.get('/api/v1/pets/owner', (req, res) => {
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
-
+const {name} = req.params;
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
-
+res.send(pet);
     // send the pet as a response
 
 });
